429c384e13347958b1f38a516469fe616c109a49
Mihail <liahim.veahcen@gmail.com>
2024-02-25 18:53:02 +0300

    make js create cards and delete

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Проектная работа Mesto
 
+https://github.com/veahcen/mesto-project-ff.git
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,25 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
-// @todo: DOM узлы
+// @todo: DOM узлы 
+const cardsList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
+function createCards(item, deleteCard) {
+  const cardContainer = cardTemplate.querySelector('.card').cloneNode(true);
+  cardContainer.querySelector('.card__title').textContent = item.name;
+  cardContainer.querySelector('.card__image').src = item.link;
+
+  const del = cardContainer.querySelector('.card__delete-button');
+  del.addEventListener('click', deleteCard);
+
+  cardsList.append(cardContainer);
+}
 
 // @todo: Функция удаления карточки
+function deleteCard(event) {
+  event.target.parentElement.remove();
+}
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach((item) => createCards(item, deleteCard));
