34231459d9e63b200454a93c0512e30de63b863a
Mihail <liahim.veahcen@gmail.com>
2024-02-25 20:02:34 +0300

    js fix

--- scripts/index.js
+++ scripts/index.js
@@ -2,24 +2,28 @@
 const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы 
-const cardsList = document.querySelector('.places__list');
+const cardsContainer = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
 function createCards(item, deleteCard) {
   const cardContainer = cardTemplate.querySelector('.card').cloneNode(true);
   cardContainer.querySelector('.card__title').textContent = item.name;
   cardContainer.querySelector('.card__image').src = item.link;
+  cardContainer.querySelector('.card__image').alt = item.name;
 
-  const del = cardContainer.querySelector('.card__delete-button');
-  del.addEventListener('click', deleteCard);
+  const buttonDelete = cardContainer.querySelector('.card__delete-button');
+  buttonDelete.addEventListener('click', () => deleteCard(cardContainer));
 
-  cardsList.append(cardContainer);
+  return cardContainer;
 }
 
 // @todo: Функция удаления карточки
-function deleteCard(event) {
-  event.target.parentElement.remove();
+function deleteCard(card) {
+  card.remove();
 }
 
 // @todo: Вывести карточки на страницу
-initialCards.forEach((item) => createCards(item, deleteCard));
+initialCards.forEach((item) => {
+  const card = createCards(item, deleteCard);
+  cardsContainer.append(card);
+});
